2f28f0b9e6ba30b9d6ddfc9c41c4d5b28b97d1b3
prerna <prernapalkar25@gmail.com>
2026-04-02 10:58:59 +0530

    Fix missing newline at end of program.cs
    Add a newline at the end of the file.

---
+++ basic/program to find max num/program.cs
@@ -0,0 +1,12 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        int a = 10, b = 20;
+        if (a > b)
+            Console.WriteLine(a + " is max");
+        else
+            Console.WriteLine(b + " is max");
+    }
+}
