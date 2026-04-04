294cc99059a6052ad44a572d7c8e22ba7abb8867
prerna <prernapalkar25@gmail.com>
2026-04-04 23:24:30 +0530

    Add program to print prime numbers in a range

---
+++ basic/Program to Print Prime Numbers in a Range/program.cs
@@ -0,0 +1,29 @@
+using System;
+
+class PrimeRange
+{
+    static void Main()
+    {
+        int start, end;
+
+        Console.Write("Enter start: ");
+        start = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter end: ");
+        end = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = start; i <= end; i++)
+        {
+            int count = 0;
+
+            for (int j = 1; j <= i; j++)
+            {
+                if (i % j == 0)
+                    count++;
+            }
+
+            if (count == 2)
+                Console.WriteLine(i);
+        }
+    }
+}
