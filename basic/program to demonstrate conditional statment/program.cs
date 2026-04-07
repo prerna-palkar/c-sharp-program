4de7eaf64eb6b2926b63b4d6dc3fa7f88334f926
prerna <prernapalkar25@gmail.com>
2026-04-07 16:39:58 +0530

    Add program to demonstrate conditional statements

---
+++ basic/program to demonstrate conditional statment/program.cs
@@ -0,0 +1,10 @@
+using System;
+class Program {
+    static void Main() {
+        int num = 10;
+        if (num > 0)
+            Console.WriteLine("Positive");
+        else
+            Console.WriteLine("Negative");
+    }
+}
