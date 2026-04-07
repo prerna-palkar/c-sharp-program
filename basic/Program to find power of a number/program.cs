ddb0cbecbe92b1464acf7ef7db487fdaf6ea5d6c
prerna <prernapalkar25@gmail.com>
2026-04-07 16:49:33 +0530

    Add program to calculate power of a number

---
+++ basic/Program to find power of a number/program.cs
@@ -0,0 +1,8 @@
+using System;
+class Program {
+    static void Main() {
+        int num = 2, power = 3;
+        double result = Math.Pow(num, power);
+        Console.WriteLine(result);
+    }
+}
