3fd71dfde28c3dccb76fb96410cfe6cfd007528a
prerna <prernapalkar25@gmail.com>
2026-04-07 16:36:30 +0530

    Add program to demonstrate type casting in C#

---
+++ basic/program to demonstrate type casting /program.cs
@@ -0,0 +1,9 @@
+using System;
+class Program {
+    static void Main() {
+        double num = 9.78;
+        int n = (int)num;  // explicit casting
+        Console.WriteLine("Double: " + num);
+        Console.WriteLine("Integer: " + n);
+    }
+}
