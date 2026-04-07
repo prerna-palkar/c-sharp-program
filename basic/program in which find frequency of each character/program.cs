00c017a46bd3438637f6fce502c859623f321c45
prerna <prernapalkar25@gmail.com>
2026-04-07 17:00:20 +0530

    Add program to find frequency of each character

---
+++ basic/program in which find frequency of each character/program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+class Program {
+    static void Main() {
+        string str = "hello";
+
+        Dictionary<char, int> freq = new Dictionary<char, int>();
+
+        foreach (char c in str) {
+            if (freq.ContainsKey(c))
+                freq[c]++;
+            else
+                freq[c] = 1;
+        }
+
+        foreach (var item in freq)
+            Console.WriteLine(item.Key + " : " + item.Value);
+    }
+}
