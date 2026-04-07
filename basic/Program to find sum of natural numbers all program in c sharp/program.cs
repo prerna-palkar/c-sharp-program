3652c1d723397ebe432f71d52d67aa8db89dd2ee
prerna <prernapalkar25@gmail.com>
2026-04-07 16:52:17 +0530

    Add program to calculate sum of natural numbers

---
+++ basic/Program to find sum of natural numbers all program in c sharp/program.cs
@@ -0,0 +1,10 @@
+using System;
+class Program {
+    static void Main() {
+        int n = 10, sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        Console.WriteLine("Sum: " + sum);
+    }
+}
