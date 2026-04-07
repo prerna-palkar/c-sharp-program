9e2258937316dc492b69538466345eaf13a731f9
prerna <prernapalkar25@gmail.com>
2026-04-07 16:50:29 +0530

    Add program to calculate student grades

---
+++ basic/program to calculate grade of the student/program.cs
@@ -0,0 +1,11 @@
+using System;
+class Program {
+    static void Main() {
+        int marks = 85;
+
+        if (marks >= 90) Console.WriteLine("A");
+        else if (marks >= 75) Console.WriteLine("B");
+        else if (marks >= 50) Console.WriteLine("C");
+        else Console.WriteLine("Fail");
+    }
+}
