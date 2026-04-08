52611d39fdacee4b807b1021c2edf4a2a1b7d36f
prerna <prernapalkar25@gmail.com>
2026-04-08 11:38:55 +0530

    Implement Bubble Sort algorithm in program.cs

---
+++ basic/program to implement Sort Array (Bubble Sort)/program.cs
@@ -0,0 +1,19 @@
+using System;
+class Program {
+    static void Main() {
+        int[] arr = {5, 2, 9, 1};
+
+        for (int i = 0; i < arr.Length - 1; i++) {
+            for (int j = 0; j < arr.Length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        foreach (int num in arr)
+            Console.Write(num + " ");
+    }
+}
