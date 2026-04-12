6f67764807cbbe2ad38b8bc25f9dda19ea97d284
prerna <prernapalkar25@gmail.com>
2026-04-12 10:56:03 +0530

    Add program to remove white spaces from a string

---
+++ advanced/Program to remove white spaces from string/program.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program {
+    static void Main() {
+        string str = "Hello World";
+        string result = str.Replace(" ", "");
+
+        Console.WriteLine(result);
+    }
+}
