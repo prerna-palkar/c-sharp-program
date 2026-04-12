2e2c0f7c5d563ae113849b37ead076a9f530d5c7
prerna <prernapalkar25@gmail.com>
2026-04-12 10:56:55 +0530

    Add program to replace substring in a string

---
+++ advanced/Program to replace substring in string/program.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program {
+    static void Main() {
+        string str = "I love Java";
+        string result = str.Replace("Java", "C#");
+
+        Console.WriteLine(result);
+    }
+}
