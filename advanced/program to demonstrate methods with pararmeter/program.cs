3e09ea9d651e5738d95db13a58199e2cba232b05
prerna <prernapalkar25@gmail.com>
2026-04-15 10:44:01 +0530

    Create program.cs

---
+++ advanced/program to demonstrate methods with pararmeter/program.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Display(string name)
+    {
+        Console.WriteLine("Hello " + name);
+    }
+
+    static void Main()
+    {
+        Display("Prerna");
+    }
+}
