f348b29eb3f75f8217f9599f020b746c7d20e3c6
prerna <prernapalkar25@gmail.com>
2026-04-21 10:40:34 +0530

    Create Fibonacci using Recursion

---
+++ advanced/Program to generate Fibonacci using recursion/program.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Fibonacci
+{
+    static int Fib(int n)
+    {
+        if (n <= 1) return n;
+        return Fib(n - 1) + Fib(n - 2);
+    }
+
+    static void Main()
+    {
+        int n = 6;
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(Fib(i) + " ");
+        }
+    }
+}
