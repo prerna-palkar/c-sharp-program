793a01995bc4526f7723740f7b5d77814ff872e8
prerna <prernapalkar25@gmail.com>
2026-04-29 11:25:23 +0530

    create demo
    multidimensional array program in c sharp

---
+++ advanced/Program to demonstrate multidimensional arrays/program.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Demo {
+    static void Main() {
+        int[,] arr = { {1,2}, {3,4} };
+
+        for(int i=0;i<2;i++)
+            for(int j=0;j<2;j++)
+                Console.Write(arr[i,j] + " ");
+    }
+}
