92c0054017348950a3448b613d9e9bea79a7d597
prerna <prernapalkar25@gmail.com>
2026-04-29 11:11:37 +0530

    Create program.Convert Case

---
+++ advanced/Program to convert string to uppercase/program.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Demo {
+    static void Main() {
+        string str = "HeLLo";
+
+        Console.WriteLine("Upper: " + str.ToUpper());
+        Console.WriteLine("Lower: " + str.ToLower());
+    }
+}
