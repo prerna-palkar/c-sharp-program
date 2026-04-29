a76dbf46de07304023151d5edbe26debd84fa5d1
prerna <prernapalkar25@gmail.com>
2026-04-29 11:26:56 +0530

    Create program.String Formatting

---
+++ advanced/Program to demonstrate string formatting/program.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Demo {
+    static void Main() {
+        string name = "Prerna";
+        int marks = 90;
+
+        Console.WriteLine($"Name: {name}, Marks: {marks}");
+    }
+}
