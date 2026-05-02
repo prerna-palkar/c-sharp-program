b3d9d91345f6c8fe05db65bb439f52575d9dc51c
prerna <prernapalkar25@gmail.com>
2026-05-02 11:59:29 +0530

    Create program.cs

---
+++ highly advanced/Program to demonstrate inheritance/program.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Animal {
+    public void Eat() {
+        Console.WriteLine("Eating...");
+    }
+}
+
+class Dog : Animal {
+    public void Bark() {
+        Console.WriteLine("Barking...");
+    }
+}
+
+class Program {
+    static void Main() {
+        Dog d = new Dog();
+        d.Eat();
+        d.Bark();
+    }
+}
