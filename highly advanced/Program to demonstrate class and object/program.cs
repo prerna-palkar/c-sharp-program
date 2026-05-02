69e46e04850db3a4fa57d9b407ce33dacd550e46
prerna <prernapalkar25@gmail.com>
2026-05-02 11:56:40 +0530

    Create program.cs

---
+++ highly advanced/Program to demonstrate class and object/program.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Student {
+    public string name;
+    public void Display() {
+        Console.WriteLine("Name: " + name);
+    }
+}
+
+class Program {
+    static void Main() {
+        Student s = new Student();
+        s.name = "Prerna";
+        s.Display();
+    }
+}
