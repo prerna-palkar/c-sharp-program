2d34ebe1d427b155a9d0ccc80a1ab63b8c10b1c3
prerna <prernapalkar25@gmail.com>
2026-05-02 12:00:14 +0530

    Create program.cs

---
+++ highly advanced/Program to demonstrate method overriding/program.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Animal {
+    public virtual void Sound() {
+        Console.WriteLine("Animal sound");
+    }
+}
+
+class Dog : Animal {
+    public override void Sound() {
+        Console.WriteLine("Dog barks");
+    }
+}
+
+class Program {
+    static void Main() {
+        Animal a = new Dog();
+        a.Sound();
+    }
+}
